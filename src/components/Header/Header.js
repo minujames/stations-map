be11c96893c474a890ac355d420c2d33e57af417
minujames <minuemiliajames@gmail.com>
2018-01-28 12:32:48 -0800

    adding header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () =>  
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <span class="navbar-brand">VOLTA</span>
+      </div>
+    </div>
+  </nav>
+
+export default Header;
+
+
+
